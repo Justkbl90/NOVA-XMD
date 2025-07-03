1b33db2fd0ff3a47869cfe588187876ba6f14e23
Justkbl90 <officialkirk69@gmail.com>
2025-07-03 09:23:39 +0000

    Update config.js

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ DELETE_LINKS: process.env.DELETE_LINKS || "true",
 // automatic delete links witho remove member 
 OWNER_NUMBER: process.env.OWNER_NUMBER || "233536385545",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "nova-xmd",
+OWNER_NAME: process.env.OWNER_NAME || "lifewithKBL",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𝙱.𝙼.𝙱-𝚇𝙼𝙳*",
 // add bot owner name    
@@ -71,7 +71,7 @@ AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
 READ_CMD: process.env.READ_CMD || "false",
 // true if want mark commands as read 
-DEV: process.env.DEV || "255767862457",
+DEV: process.env.DEV || "233536385545",
 //replace with your whatsapp number        
 ANTI_VV: process.env.ANTI_VV || "true",
 // true for anti once view 
